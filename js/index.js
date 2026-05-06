360f182de11c7d0b9d461b64f1c659503da4f879
MohianAuion <ahmohian07.gmail.com>
2026-05-06 13:19:56 +0600

    initial stage done

--- js/index.js
+++ js/index.js
@@ -4,16 +4,81 @@ const loadLevel = () => {
     .then((json) => displayLesson(json.data));
 };
 
+
+const removeActive = ()=>{
+
+  const lessonBtn = document.querySelectorAll('.lesson-btn');
+ lessonBtn.forEach((btn)=> btn.classList.remove("active"));
+}
+
+
+
 const wordPress = (id) => {
   const ul = `https://openapi.programming-hero.com/api/level/${id}`;
   fetch(ul)
     .then((res) => res.json())
-    .then((content) =>{displayWordPress(content.data);
+    .then((content) =>{
+      
+      removeActive();
+const clickBtn = document.getElementById(`lesson-btn-${id}`);
+clickBtn.classList.add("active");
+
+// console.log(clickBtn);
+     
+    displayWordPress(content.data);
     });
 };
 
-// const clickBtn = document.getElementById(`lesson-btn ${id};`);
-      //  clickBtn.classList.add('active');
+const wordDetail =async(id) =>{
+
+  const url = `https://openapi.programming-hero.com/api/word/${id}`;
+
+  const res = await fetch(url);
+  const details = await res.json();
+displayWordDetail(details.data)
+ 
+}
+
+const displayWordDetail = (word) =>{
+const wordDD = document.getElementById('wordDD');
+wordDD.innerHTML = `
+
+
+
+<div class="text-3xl font-bold">
+          <h2>
+            ${word.word} (<i class="fa-solid fa-microphone-lines"></i>: ${word.pronunciation})
+         </h2>
+    </div>
+     <div>
+          <h2 class = "text-lg font-bold">
+            Meaning
+         </h2>
+         <p class="font-medium">
+          ${word.meaning}
+         </p>
+    </div>
+     <div>
+          <h2 class = "text-lg font-bold">
+            Example
+         </h2>
+         <p class="font-medium">
+          ${word.sentence}        
+         </p>
+    </div>
+     <div>
+          <h2 class = "text-lg font-bold">
+            সমার্থক শব্দ গুলো
+         </h2>
+         <p class = "btn font-medium">${word.synonyms[0]}</p>
+         <p class = "btn font-medium">${word.synonyms[1]}</p>
+         <p class = "btn font-medium">${word.synonyms[2]}</p>
+    </div>
+
+`
+
+document.getElementById('wordModal').showModal();
+}
 
 const displayWordPress = (words) => {
   const vocabConst = document.getElementById("vocab-words");
@@ -46,9 +111,16 @@ const displayWordPress = (words) => {
         <div class="text-xl font-bold text-gray-600 font-bangla" >"${word.meaning ? word.meaning : "Meaning পাওয়া যায়নি"  } / ${word.pronunciation ? word.pronunciation : "Pronunciation পাওয়া যায়নি"  }"</div>
 
               <!-- icons -->
+              
       <div class="flex justify-between mx-10 items-center pt-5 ">
-<i class="fa-solid fa-circle-info bg-[#1A91FF20] p-2 hover:bg-[#1A91FF70] rounded text-gray-700"></i>
+
+<button onclick="wordDetail(${word.id})">
+<i class="fa-solid fa-circle-info bg-[#1A91FF20] p-2 hover:bg-[#1A91FF70] rounded text-gray-700"></i> </button>
+
+<button>
 <i class="fa-solid fa-volume-high bg-[#1A91FF20] p-2 hover:bg-[#1A91FF70] rounded text-gray-700"></i>
+</button>
+
       </div>
       </div>
 
@@ -63,7 +135,7 @@ const displayLesson = (lessons) => {
   for (let lesson of lessons) {
     const levelDiv = document.createElement("div");
     levelDiv.innerHTML = `
-                  <button id ="lesson-btn ${lesson.level_no}" onclick="wordPress(${lesson.level_no})" class="btn btn-outline btn-primary"
+                  <button id ="lesson-btn-${lesson.level_no}" onclick="wordPress(${lesson.level_no})" class="btn btn-outline btn-primary lesson-btn"
                   ><i class="fa-solid fa-book-open"></i> Lesson ${lesson.level_no}
                   </button>
 `;
